75c530b6973105cfda1ce3fd0dd7c3eed08619f8
Devcujx2112 <Vutungduongnight@gmail.com>
2025-11-03 23:47:20 +0700

    Enhancing Safe API call

--- mvvm_clean/core/data/lib/network/api_safe_call.dart
+++ mvvm_clean/core/data/lib/network/api_safe_call.dart
@@ -1,12 +1,17 @@
 import 'package:dartz/dartz.dart';
+import 'package:data/network_info/network_info.dart';
 import 'package:domain/model/failure.dart';
 import 'package:domain/model/localised_message.dart';
 
-Future<Either<Failure, T>> safeApiCall<T>(Future<T> Function() apiCall) async {
-  try {
+Future<Either<Failure, T>> safeApiCall<T>(NetworkInfo networkInfo, Future<T> Function() apiCall) async {
+  if(await networkInfo.isConnected){
+    try {
     final response = await apiCall();
     return Right(response);
   } catch (e) {
     return Left(Failure(code: '0', message: LocalisedMessage(english: '', vietnamese: '')));
+  }
+  } else {
+    return Left(Failure(code: 'NO_INTERNET', message: LocalisedMessage(english: 'No internet connection', vietnamese: 'Không có kết nối internet')));
   }
 }
---
+++ mvvm_clean/core/data/lib/network_info/network_info_impl.dart
@@ -0,0 +1,11 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:data/network_info/network_info.dart';
+
+class NetworkInfoImpl implements NetworkInfo{
+  @override
+  Future<bool> get isConnected async {
+    final connectivityResult = await Connectivity().checkConnectivity();
+    final isConnectedToNetwork = connectivityResult == ConnectivityResult.wifi || connectivityResult == ConnectivityResult.mobile;
+    return isConnectedToNetwork;
+  }
+}
